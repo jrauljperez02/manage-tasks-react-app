82997a7f0e540b3240b3cc41cfec594f8aa48bd7
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-06-09 07:51:30 -0500

    delete initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: 'task1',
+    title: "",
+    task: "",
+    setDate: new Date(2020, 7, 14),
+    finishedDate: new Date(2020,7, 15),
+  },
+  
+];
+
+const App =() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
